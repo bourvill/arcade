abaf02dd600f4d1500cd0944834b8d567b50c3eb
Maxime Marinel <bourvill@users.noreply.github.com>
2025-03-06 15:55:12 +0100

    Init

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,22 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+    app.get(".well-known", "apple-app-site-association") { req -> Response in
+        let content = """
+{"applinks":{"details":[{"appIDs":["34Z37D37V7.com.wds.bellagio"],"paths":["/en/connector"]}]}, "appclips": {
+        "apps": ["34Z37D37V7.com.wds.bellagio"]
+    }}
+"""
+        var headers = HTTPHeaders()
+        headers.add(name: .contentType, value: "application/json")
+        return Response(status: .ok, headers: headers, body: .init(string: content))
+    }
+    app.get("en", "connector") { req async -> String in
+        let id: String = req.query["id"]!
+        return "Hello, world! \(id)"
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.get("RbAcFqHw") { req async -> Response in
+        req.redirect(to: "/en/connector?id=FR*HPC*SNF001019*002")
     }
 }
